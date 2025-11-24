179a92279276c3c187ec73bc704e36fc08c27210
Andrew <akolomiec982@gmail.com>
2025-11-24 21:17:11 +0200

    Delete last name and logic connected with it

--- user/models.py
+++ user/models.py
@@ -3,15 +3,14 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
     age = models.IntegerField()
 
     def __str__(self) -> str:
-        return self.last_name + " " + self.name
+        return self.name
 
     def say_hi(self) -> str:
         # age < 18 Hi, self.name
         # age > 18 Hi, self.name self.last_name
         if self.age < 18:
             return f"Hi, {self.name}"
-        return f"Hi, {self.name} {self.last_name}"
+        return f"Hello, {self.name}"
