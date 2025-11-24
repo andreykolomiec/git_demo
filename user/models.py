3820abb387f98bb05b190a6ff9af90e6f542cae9
Andrew <akolomiec982@gmail.com>
2025-11-24 21:00:56 +0200

    Add age field from main branch

--- user/models.py
+++ user/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 
-
 class User(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
 
     def __str__(self) -> str:
         return self.last_name + " " + self.name
